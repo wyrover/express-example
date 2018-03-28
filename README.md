a65eeeba290cda2a36aa7106b02307eeae8392f9
wyrover <wyrover@gmail.com>
2018-03-28 18:59:46 +0800

    + 添加了加解密 post 数据

--- README.md
+++ README.md
@@ -14,6 +14,15 @@ rm -rf .git
 ```
 
 
+**features**
+
+1. 获取 post 二进制流并解密
+
+在使用 bodyParser.raw 的情况下，需要放在 bodyParser.json() 和 bodyParser.urlencoded 之前
+还可以通过 connect-multiparty 模块获取二进制流
+
+
+
 # links
 
 [安装 nvm 和 nrm](https://blog.csdn.net/Tyro_java/article/details/51232458)
--- express-rest-es2017-boilerplate/src/config/express.js
+++ express-rest-es2017-boilerplate/src/config/express.js
@@ -11,6 +11,11 @@ const { logs } = require('./vars');
 const strategies = require('./passport');
 const error = require('../api/middlewares/error');
 
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart();
+
+const crypto = require('crypto');
+
 /**
 * Express instance
 * @public
@@ -20,9 +25,37 @@ const app = express();
 // request logging. dev: console | production: file
 app.use(morgan(logs));
 
+// bodyParser.raw 必须放在最前面，不然使用不了
+app.use(bodyParser.raw({ type: '*/*', limit: "300mb",
+  verify: function(req, res, buf, encoding) {
+    console.log('111111111');
+    if (buf && buf.length) {
+      req.rawBody = buf;    
+      console.log('111111111');
+    }
+  }
+}));
+
+
+//app.use('/upload1', (req, res) => {
+//  let buffer = [];
+//
+//  req.on('data', (chunk) => {
+//      buffer.push(chunk);
+//  });
+//
+//  req.on('end', () => {
+//      req.rawBody = Buffer.concat(buffer);
+//      res.end(req.rawBody);
+//  });
+//
+//});
+
 // parse body params and attache them to req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 // gzip compression
 app.use(compress());
@@ -42,6 +75,86 @@ app.use(passport.initialize());
 passport.use('jwt', strategies.jwt);
 passport.use('facebook', strategies.facebook);
 passport.use('google', strategies.google);
+
+app.get('/', (req, res) => {
+  res.send('111111111111111122222222');
+});
+
+app.get('/test1', (req, res) => {
+  res.json({
+    message: 'Hello World!'
+  });
+});
+
+// 利用 connect-multiparty 解析二进制数据
+app.post('/upload', multipartMiddleware, (req, res) => {
+  console.log('isBuffer:', Buffer.isBuffer(req.body));
+  res.send(req.body);
+});
+
+
+
+
+
+
+// 利用 bodyParser.raw() 解析二进制数据
+app.post('/upload1', (req, res) => {  
+
+  console.log('isBuffer:', Buffer.isBuffer(req.rawBody));
+
+  
+  //res.send(req.rawBody);
+
+  //const key = Buffer.from([0x4B,0xD9,0xDE,0x08,0xA5,0xA7,0x49,0xAD,0x81,0x92,0xE0,0x8E,0xDC,0x54,0x24,0xD3]);
+  var publicKey = "12345678";
+  var key = crypto.createHash("md5").update(publicKey).digest('hex');
+
+  console.log("md5: %s", key);
+  var iv = "";
+  
+  var clearEncoding = 'binary';
+  var cipherEncoding = 'binary';
+  var cipherChunks = [];
+  var cipher = crypto.createCipheriv('aes-256-ecb', key, iv);
+  cipher.setAutoPadding(true);
+
+  
+
+  cipherChunks.push(cipher.update(req.rawBody, clearEncoding, cipherEncoding));
+  cipherChunks.push(cipher.final(cipherEncoding));  
+
+  
+
+  //res.send(cipherChunks.join(''));
+
+  var source = new Buffer(cipherChunks.join(''), 'binary');
+
+
+  //res.send(source);
+
+  var decrypted = [];
+  var decipher = crypto.createDecipheriv('aes-256-ecb', key, iv);
+  decipher.setAutoPadding(true);
+  decrypted.push(decipher.update(source, 'buffer', 'binary'));
+  decrypted.push(decipher.final('binary'));
+//
+  res.send(decrypted.join(''));
+
+  
+
+  
+});
+
+
+
+
+
+app.post('/upload2', (req, res, next) => {  
+
+  console.log('isBuffer:', Buffer.isBuffer(req.body));
+
+  res.send(req.body);
+});
 
 // mount api v1 routes
 app.use('/v1', routes);
